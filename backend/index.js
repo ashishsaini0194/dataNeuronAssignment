221a4b9b0da7312beb5395df234b628357b6f639
ashishCloudesign <ashish.saini@cloudesign.com>
2024-04-10 17:36:46 +0530

    added vercel config

--- backend/index.js
+++ backend/index.js
@@ -106,3 +106,6 @@ app.put('/update/:id', async (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+
+module.exports = app; //for vercel dep
